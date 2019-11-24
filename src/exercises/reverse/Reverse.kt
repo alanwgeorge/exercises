bc862ccce75601737a7d38909042427637fdf8c3
Alan George <ageorge@alan-george.com>
2019-11-24 11:52:40 -0800

    reverse

---
+++ src/exercises/reverse/Reverse.kt
@@ -0,0 +1,29 @@
+package exercises.reverse
+
+/*
+* https://www.hackerearth.com/practice/algorithms/string-algorithm/basics-of-string-manipulation/practice-problems/algorithm/terrible-chandu/
+*/
+
+fun main() {
+    val inputCount = readLine()?.toInt() ?: throw Exception("error parsing input line count")
+    repeat(inputCount) {
+        val input = readLine()?.toCharArray() ?: throw Exception("error parsing input line")
+//        println("input: ${input.toList()}")
+
+        var s = 0
+        var e = input.size - 1
+
+        while (e - s >= 0){
+//            println("${input[s]} ${input[e]} ${e - s}")
+            val temp = input[s]
+
+            input[s] = input[e]
+            input[e] = temp
+
+            s++
+            e--
+        }
+
+        println(String(input))
+    }
+}
