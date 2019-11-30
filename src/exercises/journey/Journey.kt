be710444cd4b1af8c30428e13be06fb6fedd85a4
alanwgeorge <ageorge@alan-george.com>
2019-11-30 09:06:22 -0800

    journey update

--- src/exercises/journey/Journey.kt
+++ src/exercises/journey/Journey.kt
@@ -17,24 +17,19 @@ fun main() {
             val fuelCost = readLine()?.split(" ", limit = checkpoints)?.map { it.toLong() }?.toTypedArray() ?: throw Exception("error parsing fuel costs")
             val checkpointFuelConsumption = readLine()?.split(" ", limit = checkpoints)?.map { it.toLong() }?.toTypedArray() ?: throw Exception("error parsing next checkpoint fuel consumption")
 
-            println(calculateJourneyMinCost(fuelCost, checkpointFuelConsumption))
+            var totalCost = 0L
+            var minCost = Long.MAX_VALUE
+            for (curCheckpoint in fuelCost.indices) {
+                if (fuelCost[curCheckpoint] < minCost) {
+                    minCost = fuelCost[curCheckpoint]
+                }
+
+                totalCost += minCost * checkpointFuelConsumption[curCheckpoint]
+            }
+
+            println(totalCost)
         }
     }
-
     println("measure = ${NumberFormat.getInstance().format(measure)}")
 }
 
-fun calculateJourneyMinCost(fuelCost: Array<Long>, checkpointFuelConsumption: Array<Long> ): Long {
-    var totalCost = 0L
-    var minCost = Long.MAX_VALUE
-
-    for (curCheckpoint in 0..(fuelCost.size - 1)) {
-        if (fuelCost[curCheckpoint] < minCost) {
-            minCost = fuelCost[curCheckpoint]
-        }
-
-        totalCost += minCost * checkpointFuelConsumption[curCheckpoint]
-    }
-
-    return totalCost
-}
