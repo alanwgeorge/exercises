77e0c2adc740c7a8a4a1c983af8e4ef7226ac170
Alan George <ageorge@alan-george.com>
2020-01-30 15:22:11 -0800

    merge K sorted lists

--- src/exercises/mergeksortedlists/MergeKSortedLists.kt
+++ src/exercises/mergeksortedlists/MergeKSortedLists.kt
@@ -10,29 +10,68 @@ class ListNode(var `val`: Int) {
     override fun toString(): String {
         return "ListNode(`val`=$`val`, next=$next)"
     }
-}
 
-class Solution {
-    fun mergeKLists(lists: Array<ListNode?>): ListNode? {
-        val result = null
+    fun toList(): List<Int> {
+        val result = mutableListOf<Int>()
+        var ptr: ListNode? = next
+        result.add(`val`)
+
+        do {
+            ptr?.`val`?.let { result.add(it) }
+            ptr = ptr?.next
+        } while (ptr != null)
 
         return result
+    }
+}
+
+class Solution(private val debug: Boolean = false) {
+    companion object {
+        var loops = 0
+    }
+
+    fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+        val root = ListNode(-1)
+        var tail: ListNode? = root
+
+        while (lists.filterNotNull().isNotEmpty()) {
+            loops++
+            var lowest: ListNode? = null
+            var lowestIdx = -1
+            lists.forEachIndexed { idx, node ->
+                loops++
+                node?.let {
+                    if (debug) println("$idx $node $lowest")
+                    if (it.`val` < lowest?.`val` ?: Int.MAX_VALUE) {
+                        lowest = it
+                        lowestIdx = idx
+                    }
+                }
+            }
+            lists[lowestIdx] = lowest?.next
+            tail?.next = lowest
+            tail = lowest
+        }
+
+        return root.next
     }
 }
 
 
 fun main() {
     val tests = listOf(
-            listOf(listOf(1,4,5), listOf(1,3,4), listOf(2,6)) to listOf(1,1,2,3,4,4,5,6)
-    )
+            listOf(listOf(1,4,5), listOf(1,3,4), listOf(2,6)) to listOf(1,1,2,3,4,4,5,6),
+            listOf(listOf(1,2,3), listOf(5,6,7), listOf(9,10)) to listOf(1,2,3,5,6,7,9,10)
+            )
 
-    val s = Solution()
+    val s = Solution(debug = true)
 
     tests.forEach {
         println("test: $it")
+        Solution.loops = 0
         val result = s.mergeKLists(it.first.toArrayListNode())
-        println(result)
-        if (result != it.second.toListNode()) throw Exception("test $it failed, result $result, expected ${it.second.toListNode()}")
+        println("${result?.toList()} loops:${Solution.loops}")
+        if (result?.toList() != it.second) throw Exception("test $it failed, result ${result?.toList()}, expected ${it.second}")
     }
 }
 
